d9b659419b8d73353249e4493b8d9f7ccad7e603
spacehun <spacesus@proton.me>
2022-12-29 17:53:17 +0100

    implementar info de podcast

--- src/pages/PodcastLayout.jsx
+++ src/pages/PodcastLayout.jsx
@@ -1,10 +1,38 @@
-import { Outlet } from 'react-router-dom'
+import { fetchPodcast } from '../api.js'
+import { Link, Outlet, useParams } from 'react-router-dom'
+import { useQuery } from 'react-query'
 
-const PodcastLayout = () => (
-	<>
-		<h1>Podcast</h1>
-		<Outlet/>
-	</>
-)
+const PodcastLayout = () => {
+
+	const { podcast } = useParams()
+
+	// Descargar
+	const { data, isError, isLoading } = useQuery({
+		queryKey: ['podcast', podcast],
+		queryFn: () => fetchPodcast(podcast),
+		cacheTime: 1000 * 60 * 60 * 24,
+	})
+	if (isLoading)
+		return (
+			<p>Cargando</p>
+		)
+	const {title, author, description, image} = data
+
+	// Renderizar
+	return (
+		<div>
+			<div>
+				<Link to={`/podcast/${podcast}`}>
+					<img src={image} alt=""/>
+					<h1>{title}</h1>
+					<p>{'by ' + author}</p>
+				</Link>
+				<h3>Description:</h3>
+				<p>{description}</p>
+			</div>
+			<Outlet/>
+		</div>
+	)
+}
 
 export default PodcastLayout
