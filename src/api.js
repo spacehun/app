1a099cf312739a7302f3cd8aa7b0d5f8ae38a970
spacehun <spacesus@proton.me>
2022-12-29 17:14:05 +0100

    consultar lisdta de episodios + episodio en la api

--- src/api.js
+++ src/api.js
@@ -1,3 +1,10 @@
+async function proxy(url) {
+	const proxy =`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`
+	const response = await fetch(proxy)
+	const data = await response.json()
+	return JSON.parse(data.contents)
+}
+
 /**
  * Listado de podcasts
  */
@@ -24,15 +31,54 @@ export async function fetchPodcasts() {
 export async function fetchPodcast(podcast) {
 
 	const url =`https://itunes.apple.com/lookup?id=${podcast}`
-	const proxy =`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`
-	const response = await fetch(proxy)
-	const data = await response.json()
-	const item = JSON.parse(data.contents).results[0]
+	const data = await proxy(url) 
+	const item = data.results[0]
 	return {
 		title: item.collectionName,
 		author: item.artistName,
 		// La descripción no aparece
 		description: item.genres.join(', '),
 		image: item.artworkUrl100,
+	}
+}
+
+/**
+ * Episodios de un podcast
+ */
+export async function fetchEpisodes(podcast) {
+
+	const url =`https://itunes.apple.com/lookup?id=${podcast}&entity=podcastEpisode`
+	const data = await proxy(url) 
+	const items = data.results.slice(1)
+	const episodes = []
+	for (const item of items) {
+		const duration = new Date(null)
+		duration.setSeconds(Math.round(item.trackTimeMillis / 1000))
+		episodes.push({
+			code: item.trackId,
+			title: item.trackName,
+			date: item.releaseDate,
+			duration: duration.toISOString().substr(11, 8),
+		})
+	}
+	return episodes
+}
+
+/**
+ * Información de un episodio
+ */
+export async function fetchEpisode(podcast, episode) {
+
+	const url =`https://itunes.apple.com/lookup?id=${podcast}&trackId=${episode}&entity=podcastEpisode`
+	const data = await proxy(url) 
+	const items = data.results
+	for (const item of items) {
+		if (item.kind === 'podcast-episode' && item.trackId == episode) {
+			return {
+				title: item.trackName,
+				description: item.description,
+				audio: item.episodeUrl,
+			}
+		}
 	}
 }
