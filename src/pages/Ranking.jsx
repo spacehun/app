d2bfab64d97e5c9b27f2c982f7b7f6387b759b38
spacehun <spacesus@proton.me>
2022-12-29 19:48:25 +0100

    implementar busqueda

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,9 +1,13 @@
+import Fuse from 'fuse.js'
 import PodcastSummary from '../components/PodcastSummary'
 import { fetchPodcasts } from '../api.js'
 import { useQuery } from 'react-query'
+import { useState } from 'react'
 import '../styles/Ranking.css'
 
 const Ranking = () => {
+
+	const [ pattern, setPattern ] = useState('')
 
 	// Descargar
 	const { data, isLoading } = useQuery({
@@ -18,12 +22,25 @@ const Ranking = () => {
 	}
 
 	// Renderizar
-	const entries = data.map(({id, name, author, image}) => (
-		<PodcastSummary id={id} name={name} author={author} image={image}/>
-	))
+	const fuse = new Fuse(data, {
+		keys: ['name', 'author'],
+	})
+	let entries
+	if (pattern) {
+		// Filtrar
+		const match = fuse.search(pattern)
+		entries = match.map(({item}) => (
+			<PodcastSummary id={item.id} name={item.name} author={item.author} image={item.image}/>
+		))
+	} else {
+		// No filtrar: mostrar todos
+		entries = data.map(({id, name, author, image}) => (
+			<PodcastSummary id={id} name={name} author={author} image={image}/>
+		))
+	}
 	return (
 		<div className="ranking">
-			<input type="text"/>
+			<input type="text" onChange={event => {setPattern(event.target.value)}}/>
 			<div>
 				{entries}
 			</div>
