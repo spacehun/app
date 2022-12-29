9f228637ec79495ad64be0bacafd018681ef3d1a
spacehun <spacesus@proton.me>
2022-12-29 16:38:17 +0100

    consultar podcast en la api

--- src/api.js
+++ src/api.js
@@ -1,3 +1,6 @@
+/**
+ * Listado de podcasts
+ */
 export async function fetchPodcasts() {
 	const URL = 'https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json'
 	const response = await fetch(URL)
@@ -13,4 +16,23 @@ export async function fetchPodcasts() {
 			image: podcast['im:image'][2].label,
 		}
 	))
+}
+
+/**
+ * Información de un podcast
+ */
+export async function fetchPodcast(podcast) {
+
+	const url =`https://itunes.apple.com/lookup?id=${podcast}`
+	const proxy =`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`
+	const response = await fetch(proxy)
+	const data = await response.json()
+	const item = JSON.parse(data.contents).results[0]
+	return {
+		title: item.collectionName,
+		author: item.artistName,
+		// La descripción no aparece
+		description: item.genres.join(', '),
+		image: item.artworkUrl100,
+	}
 }
