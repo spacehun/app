29bac86412476487211bc03a410b5f13675918dc
spacehun <spacesus@proton.me>
2022-12-29 18:19:38 +0100

    implementar episodio

--- src/pages/Episode.jsx
+++ src/pages/Episode.jsx
@@ -1,5 +1,34 @@
-const Episode = () => (
-	<h1>Episodio</h1>
-)
+import { fetchEpisode } from '../api.js'
+import { useParams } from 'react-router-dom'
+import { useQuery } from 'react-query'
+
+const Episode = () => {
+
+	const { podcast, episode } = useParams()
+
+	// Descargar
+	const { data, isLoading } = useQuery({
+		queryKey: ['episode', podcast, episode],
+		queryFn: () => fetchEpisode(podcast, episode),
+		cacheTime: 1000 * 60 * 60 * 24,
+	})
+	if (isLoading) {
+		return (
+			<p>Cargando</p>
+		)
+	}
+	const {title, description, audio} = data
+
+	// Renderizar
+	return (
+		<div>
+			<h1>{title}</h1>
+			<p>{description}</p>
+			<audio controls>
+				<source src={audio} type="audio/mpeg"/>
+			</audio>
+		</div>
+	)
+}
 
 export default Episode
